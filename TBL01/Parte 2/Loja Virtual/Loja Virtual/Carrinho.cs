3a33e8063cd3dac72f8bad078eb01c41ca515226
JPedroMB <166443434+JPedroMB@users.noreply.github.com>
2026-02-23 20:08:13 -0300

    Add files via upload

---
+++ TBL01/Parte 2/Loja Virtual/Loja Virtual/Carrinho.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LojaVirtual
+{
+    public class Carrinho
+    {
+        public List<Produto> Itens { get; set; } = new List<Produto>();
+
+        // Método para adicionar produtos (Requisito Incremento 1)
+        public void AdicionarProduto(Produto p)
+        {
+            Itens.Add(p);
+            Console.WriteLine($"Produto '{p.Nome}' adicionado.");
+        }
+
+        // Método para calcular total (Requisito Incremento 1)
+        public double CalcularTotal()
+        {
+            double total = 0;
+            foreach (var item in Itens)
+            {
+                total += item.Preco;
+            }
+            return total;
+        }
+    }
+}
